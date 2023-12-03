440fe423b5772403d6eeb4b37deeac863843073c
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2023-12-03 22:25:41 +0100

    [Feature] Adding the basic frontend and backend setup

---
+++ app/generate_carol.py
@@ -0,0 +1,7 @@
+# Carol Generation
+
+
+def generate_carol(user_input):
+    # Implement your carol generation logic here using user_input
+    # For simplicity, let's return a placeholder carol for now
+    return "We wish you a Merry Christmas!"
